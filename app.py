f69e78be8dee459931f9fe97f3f3f0f6265a1ddb
Sachinkumaransh844502 <sachinyadavyahi@gmail.com>
2025-06-19 14:54:27 +0530

    First Commit

---
+++ app.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import streamlit as st
+import seaborn as sns
+import plotly.express as px
+import plotly.graph_objects as go
+import plotly.io as pio
+import plotly.figure_factory as ff
+
